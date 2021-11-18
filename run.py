6af07ee099345665c26ccfeb7d001ee65d0c8a80
stolyarov <6968@list.ru>
2021-11-18 10:28:51 +0300

    switch on to debug mode

--- run.py
+++ run.py
@@ -2,5 +2,5 @@
 
 from app import app
 
-# app.run(debug=True, port=5001)
-app.run(host="0.0.0.0", port=8080, debug=True)
+app.run(debug=True, port=5001)
+# app.run(host="0.0.0.0", port=8080, debug=True)
