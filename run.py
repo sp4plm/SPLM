fee26719a85653c69f00bd1d0019ebbc450a24ab
stolyarov <6968@list.ru>
2021-11-18 11:01:52 +0300

    just for test

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-
+import os
 from app import app
 
-app.run(debug=True, port=5001)
-# app.run(host="0.0.0.0", port=8080, debug=True)
+# app.run(debug=True, port=5001)
+
+port = int(os.environ.get('PORT', 33507))
+app.run(host="0.0.0.0", port=port, debug=True)
