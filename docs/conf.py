cf714ce907e99c532eb45ac355314241beae4f42
maxef <Efimovskiy_MV@aep.ru>
2022-12-12 10:17:33 +0300

    rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -33,5 +33,9 @@ language = 'ru'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+pygments_style = 'sphinx'
+
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+
