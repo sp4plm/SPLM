27e743f481fd2f822e8cce84329500640826e870
maxef <Efimovskiy_MV@aep.ru>
2023-01-30 18:01:51 +0300

    тест rst

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 project_root = os.path.dirname(os.getcwd())
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, project_root)
 
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosectionlabel', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
