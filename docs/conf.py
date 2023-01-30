05ba29e771b529422e2412869e9386df0936d2e7
maxef <Efimovskiy_MV@aep.ru>
2023-01-30 16:30:47 +0300

    тест rst

--- docs/conf.py
+++ docs/conf.py
@@ -5,11 +5,9 @@ import sys
 import os
 
 project_root = os.path.dirname(os.getcwd())
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, project_root)
 
-print(os.path.abspath('.'))
-
-# need_sphinx = '1.0'
+need_sphinx = '5.3.0'
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosectionlabel', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
