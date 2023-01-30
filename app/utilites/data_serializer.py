e6a5820cb60ed3a83d66113ccf42c551920a35ca
maxef <Efimovskiy_MV@aep.ru>
2023-01-30 12:59:34 +0300

    тест rst

--- app/utilites/data_serializer.py
+++ app/utilites/data_serializer.py
@@ -5,12 +5,14 @@ class DataSerializer:
     _class_file = __file__
     _debug_name = 'DataSerializer'
 
+    @staticmethod
     def restore(self, file_name):
         fdata = None
         with open(file_name, 'rb') as fp:
             fdata = pickle.load(fp)
         return fdata
 
+    @staticmethod
     def dump(self, file_name, fdata):
         with open(file_name, 'wb') as fp:
             pickle.dump(fdata, fp, pickle.HIGHEST_PROTOCOL)
